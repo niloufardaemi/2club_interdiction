ff795c98e540cc0b8b0c267d83f1bc557686b46d
Niloufar Daemi <54544655+niloufardaemi@users.noreply.github.com>
2021-08-26 16:41:31 -0500

    Update GRBInterface.h

--- GRBInterface.h
+++ GRBInterface.h
@@ -25,27 +25,15 @@ long KNeighborhoodSize(KGraph &g, long k, long v);
 long KNeighborhoodSize(KGraph &g, vector<bool> &W, long k, long v);
 
 
-vector<long> solve2Club(KGraph &g, long k, vector<long> &HeuristicSolution, bool &subOpt);
-
 // Our cut-like formulation for max k-club. Uses the routine MINIMALIZE to strengthen the cuts.
 vector<long> solveMaxKClub_CutLike(KGraph &g, long k, vector<long> &HeuristicSolution, bool &subOpt);
 vector<long> MINIMALIZE(KGraph &g, long a, long b, vector<long> length_k_ab_separator, long k);
 
 // Combination of graph decomposition and our cut-like formulation
-//void ICUT(KGraph &g, long k, vector<long> &BestKClub);
 vector<long>  ICUT(KGraph &g, long k, vector<long> &BestKClub);
 
 vector<long> ICUT_Subproblem(KGraph &g, long k, long v, long LowerBound, long &SubProblemLB, long &SubProblemUB);
 
-// Path-like formulations. The case k=3 is the "neighborhood" formulation (F_N) of Almeida and Carvalho.
-vector<long> Pathlike_3Club(KGraph &g, vector<long> &HeuristicSolution, bool &subOpt);
-vector<long> Pathlike_4Club(KGraph &g, vector<long> &HeuristicSolution, bool &subOpt);
-
-// solve max k-club using VB formulation 
-vector<long> MaxKclubRevisedVeremyevFormulation(KGraph &g1, long k, vector<long> &HeuristicSolution, bool &subOpt);
-
-// solve max k-club with using CHC cuts in callback, as per Moradi and Balasundaram (2015).
-vector<long> solveMaxClubCHC(KGraph &g, long k, vector<long> &HeuristicSolution, bool &subOpt);
 
 // callback functions for cut-like formulation
 class Kclub_callback : public GRBCallback
@@ -68,26 +56,4 @@ public:
 };
 
 
-// Callback function for k-club using CHC cuts
-class CHC : public GRBCallback
-{
-public:
-	GRBVar *vars;
-	KGraph g1;
-	long k1;
-
-	CHC(GRBVar *xvars, KGraph &g, long k)
-	{
-		vars = xvars;
-		g1.Duplicate(g);
-		k1 = k;
-	}
-	void callback();
-	static long numCallbacks;
-	static double TotalCallbackTime;
-	static long numLazyCuts;
-};
-
-
 #endif
-
